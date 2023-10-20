4734518c6299631cd5ecc3407d5dead3612d4163
Lucas-Akiama <lucas.brasiliano@sptech.school>
2023-10-20 15:29:34 -0300

    download csv, geração e leitura do arquivo, com base na lista obj

--- src/main/java/culinart/api/funcionario/FuncionarioController.java
+++ src/main/java/culinart/api/funcionario/FuncionarioController.java
@@ -3,12 +3,19 @@ package culinart.api.funcionario;
 import culinart.domain.fornecedor.dto.FuncionarioDTO;
 import culinart.service.funcionario.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.*;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/funcionarios")
@@ -28,6 +35,28 @@ public class FuncionarioController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(funcionariosOrdenadosPorNomeDTO);
+    }
+
+    @GetMapping("/download/csv")
+    public ResponseEntity<byte[]> download(){
+        List<FuncionarioDTO> funcionariosOrdenadosPorNomeDTO = funcionarioService.funcionariosOrdenadosDTO();
+        String nomeArquivo = funcionarioService.gerarArquivoCsv(funcionariosOrdenadosPorNomeDTO);
+        try {
+            File file = new File(nomeArquivo);
+            byte[] csvContent = Files.readAllBytes(file.toPath());
+
+            // Configura a resposta HTTP
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "attachment; filename=funcionarios.csv");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.parseMediaType("text/csv"))
+                    .body(csvContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(500, "Erro ao ler ou enviar o arquivo CSV", e);
+        }
     }
 
 }
