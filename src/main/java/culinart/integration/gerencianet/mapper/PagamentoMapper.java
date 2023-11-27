4d6660336942ce816ea79038db4aed30990c22be
gabs-mvb <gabriel.boas@sptech.school>
2023-11-27 20:46:22 -0300

    retornando endpoint de ultimo pagamento por idUsuario

--- src/main/java/culinart/integration/gerencianet/mapper/PagamentoMapper.java
+++ src/main/java/culinart/integration/gerencianet/mapper/PagamentoMapper.java
@@ -4,9 +4,11 @@ import culinart.domain.pagamento.Pagamento;
 import culinart.domain.usuario.Usuario;
 import culinart.integration.gerencianet.subscription.dto.PagamentoDTO;
 
+import java.time.LocalDate;
+
 public class PagamentoMapper {
-    public static Pagamento toEntity (PagamentoDTO pagamentoDTO, Usuario usuario){
-        if (pagamentoDTO == null){
+    public static Pagamento toEntity(PagamentoDTO pagamentoDTO, Usuario usuario) {
+        if (pagamentoDTO == null) {
             return null;
         }
         return Pagamento.builder()
@@ -18,6 +20,21 @@ public class PagamentoMapper {
                 .statusTransacao(pagamentoDTO.getStatusTransacao())
                 .statusAssinatura(pagamentoDTO.getStatusAssinatura())
                 .usuario(usuario)
+                .build();
+    }
+
+    public static PagamentoDTO toDTO(Pagamento pagamento) {
+        if (pagamento == null) {
+            return null;
+        }
+
+        return PagamentoDTO.builder()
+                .idAssinatura(pagamento.getIdAssinatura())
+                .statusAssinatura(pagamento.getStatusAssinatura())
+                .idTransacao(pagamento.getIdTransacao())
+                .statusTransacao(pagamento.getStatusTransacao())
+                .linkCobranca(pagamento.getLinkCobranca())
+                .dataExpiracao(pagamento.getDataExpiracao())
                 .build();
     }
 }
--- src/main/java/culinart/integration/gerencianet/repository/PagamentoRepository.java
+++ src/main/java/culinart/integration/gerencianet/repository/PagamentoRepository.java
@@ -1,7 +1,11 @@
 package culinart.integration.gerencianet.repository;
 
 import culinart.domain.pagamento.Pagamento;
+import culinart.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+    Optional<Pagamento> findByUsuarioOrderByDataExpiracaoDesc(Usuario usuario);
 }
