bff92e5903e161a58b32eca56d1f3302c7705fc1
gabsmvb <gabriel.boas@sptech.school>
2024-03-07 20:45:26 -0300

    inserção de All args e No args

--- src/main/java/culinart/domain/assinatura/dto/AssinaturaDTO.java
+++ src/main/java/culinart/domain/assinatura/dto/AssinaturaDTO.java
@@ -1,10 +1,14 @@
 package culinart.domain.assinatura.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssinaturaDTO {
     private int idAssinatura;
     private String statusAssinatura;
