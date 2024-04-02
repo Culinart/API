2da1463d82e91e8d4f0490c17c2b4e2c1e821b3d
gabsmvb <gabriel.boas@sptech.school>
2024-04-01 21:12:54 -0300

    reestruturação do prefixo dos endpoints

--- src/main/java/culinart/api/usuario/configuration/security/SecurityConfiguracao.java
+++ src/main/java/culinart/api/usuario/configuration/security/SecurityConfiguracao.java
@@ -51,11 +51,11 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/webjars/**"),
             new AntPathRequestMatcher("/v3/api-docs/**"),
             new AntPathRequestMatcher("/actuator/*"),
-            new AntPathRequestMatcher("/usuarios/login/**"),
-            new AntPathRequestMatcher("/usuarios/cadastro/**"),
+            new AntPathRequestMatcher("/api/usuarios/login/**"),
+            new AntPathRequestMatcher("/api/usuarios/cadastro/**"),
             new AntPathRequestMatcher("/h2-console/**"),
             new AntPathRequestMatcher("/error/**"),
-            new AntPathRequestMatcher("/funcionarios/login/**"),
+            new AntPathRequestMatcher("/api/funcionarios/login/**"),
     };
 
     @Bean
